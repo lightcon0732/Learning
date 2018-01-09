b88df557fd5aea83c40fbf535df51c33d99b3cc3
Constantine Kozarev <light.con0732@gmail.com>
2018-01-09 16:38:32 +0200

    2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-alert('Я - JavaScript!');
+
+function searchPicture () {
+  var smallPicture = document.getElementsByClassName('smallPicture')
+
+  var windowWidth = window.width();
+  var windowHeight = window.height();
+
+  smallPicture.style.top = windowHeight / 2 - 100 + "px";
+  smallPicture.style.left = windowWidth / 2 - 100 + "px";
+}
